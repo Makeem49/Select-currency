44f1357f105e4c2ff52278656dbd6af3d32c3e3d
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-09-03 12:14:33 +0100

    adding side bar and reset css

--- index.js
+++ index.js
@@ -8,15 +8,52 @@ const currencyType = currency => {
 
 const render = () => {
     allBox.forEach(box => {
+        // For single selection of currency
+        /*
         box.addEventListener('click', event => {
-            console.log(event.currentTarget)
+            let query = document.querySelector('.cards .card.active').classList.remove('active') ?? document.querySelector('.cards .card')
+            console.log(query)
 
-            console.log(document.querySelector('.cards .card.active').classList.remove('active'))
+            // console.log(query.classList)
             event.currentTarget.classList.add('active')
+            currencyType(event.currentTarget)
+        })
+        */
+
+        // For multiple selection of currency
+        box.addEventListener('click', event => {
+
+            // console.log(query.classList)
+            event.currentTarget.classList.toggle('active')
             currencyType(event.currentTarget)
         })
     })
 }
 
 
+let sidebar = document.querySelector('.hamburger i')
+
+sidebar.addEventListener('click', event => {
+    let select = document.querySelector('#side-bar').classList.toggle('show')
+})
+
+
 render()
+
+document.querySelector('#button').addEventListener('click', () => {
+    // console.log(document.querySelector('button').setAttribute('disabled', 'disbaled'))
+    // console.log(document.querySelector('button').getAttribute('disabled'))
+    // console.log(document.querySelector('button').hasAttribute('disabled'))
+    // console.log(document.querySelector('button').removeAttribute('disabled'))
+
+    let changeElement = document.querySelector('body').classList.toggle('change-color');
+
+    
+    if(changeElement){
+        document.querySelector('p').remove()
+    } else {
+        let child = document.createElement('p');
+        child.textContent = 'I have ben added back but the start of the page';
+        document.documentElement.insertAdjacentElement('afterbegin', child);
+    };
+});
